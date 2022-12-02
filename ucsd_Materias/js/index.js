efd1ea05772f560e9e371bd6d217c0e917fead40
ajimenezrosa <ja.jimenezrosa@gmail.com>
2022-12-02 14:51:36 -0400

    correcion de error de carga luego de commit

--- ucsd_Materias/js/index.js
+++ ucsd_Materias/js/index.js
@@ -1,20 +1,18 @@
 $(docReady);
 const JSONURLS = {
-
-    meta: "https://ajimenezrosa.github.io/ucsd_Materias/json/1.json",
-  1: "https://ajimenezrosa.github.io/ucsd_Materias/json/1.json",
-  2: "https://ajimenezrosa.github.io/ucsd_Materias/json/2.json",
-  3: "https://ajimenezrosa.github.io/ucsd_Materias/json/3.json",
-  4: "https://ajimenezrosa.github.io/ucsd_Materias/json/4.json",
-  5: "https://ajimenezrosa.github.io/ucsd_Materias/json/5.json",
-  6: "https://ajimenezrosa.github.io/ucsd_Materias/json/6.json"
-
+  meta: "https://arigoru.github.io/fcc-curriculum-aggregator/json/meta.json",
+  1: "https://arigoru.github.io/fcc-curriculum-aggregator/json/1.json",
+  2: "https://arigoru.github.io/fcc-curriculum-aggregator/json/2.json",
+  3: "https://arigoru.github.io/fcc-curriculum-aggregator/json/3.json",
+  4: "https://arigoru.github.io/fcc-curriculum-aggregator/json/4.json",
+  5: "https://arigoru.github.io/fcc-curriculum-aggregator/json/5.json",
+  6: "https://arigoru.github.io/fcc-curriculum-aggregator/json/6.json"
 }
 let $loading = $("<div>");
 const loadingAnimation = {
   play: function () {
     $("#main-body").append($loading)
-    // $loading.addClass("loadingAnimation").attr("id", "loadingAnimation");
+    $loading.addClass("loadingAnimation").attr("id", "loadingAnimation");
     $loading.append("<h1>Loading, please wait...</h1>");
     $loading.animate({ opacity: 1 }, 2000);
   },
@@ -95,7 +93,7 @@ function parseMeta() {
     {
       slideId: 1,
       title: "Responsive Web Design",
-      meta: unparsedData.curriculumMetaRaw.filter(filterMeta("Analisis_Diseno_Algoritmos")),
+      meta: unparsedData.curriculumMetaRaw.filter(filterMeta("responsive-web-design")),
     },
     {
       slideId: 2,
