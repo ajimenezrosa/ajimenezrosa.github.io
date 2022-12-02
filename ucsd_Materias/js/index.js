e940299bd07bdf227de28c31dffc875682bb26d2
ajimenezrosa <ja.jimenezrosa@gmail.com>
2022-12-02 14:59:25 -0400

    rutas

--- ucsd_Materias/js/index.js
+++ ucsd_Materias/js/index.js
@@ -1,12 +1,12 @@
 $(docReady);
 const JSONURLS = {
-  meta: "https://arigoru.github.io/fcc-curriculum-aggregator/json/meta.json",
-  1: "https://arigoru.github.io/fcc-curriculum-aggregator/json/1.json",
-  2: "https://arigoru.github.io/fcc-curriculum-aggregator/json/2.json",
-  3: "https://arigoru.github.io/fcc-curriculum-aggregator/json/3.json",
-  4: "https://arigoru.github.io/fcc-curriculum-aggregator/json/4.json",
-  5: "https://arigoru.github.io/fcc-curriculum-aggregator/json/5.json",
-  6: "https://arigoru.github.io/fcc-curriculum-aggregator/json/6.json"
+  meta: "https://github.com/ajimenezrosa/ajimenezrosa.github.io/tree/main/ucsd_Materias/json/meta.json",
+  1: "https://github.com/ajimenezrosa/ajimenezrosa.github.io/tree/main/ucsd_Materias/json/1.json",
+  2: "https://github.com/ajimenezrosa/ajimenezrosa.github.io/tree/main/ucsd_Materias/json/2.json",
+  3: "https://github.com/ajimenezrosa/ajimenezrosa.github.io/tree/main/ucsd_Materias/json/3.json",
+  4: "https://github.com/ajimenezrosa/ajimenezrosa.github.io/tree/main/ucsd_Materias/json/4.json",
+  5: "https://github.com/ajimenezrosa/ajimenezrosa.github.io/tree/main/ucsd_Materias/json/5.json",
+  6: "https://github.com/ajimenezrosa/ajimenezrosa.github.io/tree/main/ucsd_Materias/json/6.json"
 }
 let $loading = $("<div>");
 const loadingAnimation = {
